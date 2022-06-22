4777f6a405ec560db87b5c7585f02c1a1930ed00
Saurabh Rana <>
2022-06-22 22:58:23 +0530

    Initial commit (Sub Module)

---
+++ PracticeSubModule/PracticeSubModule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeSubModule
+//
+//  Created by Saurabh Rana on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
